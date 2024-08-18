be6c981222edae795556deb99e7820c7440e239a
kos5667 <kos5667@gmail.com>
2024-08-18 17:13:54 +0900

    Q1546.java

---
+++ algorithm/BAEKJOON/A_bronze/Q1546.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 1546 | 평균 구하기
+ * 시간 제한 2초 | 메모리 제한 128 MB | 브론즈1
+ */
+public class Q1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        double[] array = new double[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for (int i=0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        double m = Arrays.stream(array).max().orElseThrow(() -> new RuntimeException("Error"));
+        double r = Arrays.stream(array).map(num -> ((num / m) * 100)).average().orElseThrow();
+        System.out.println(r);
+    }
+}
