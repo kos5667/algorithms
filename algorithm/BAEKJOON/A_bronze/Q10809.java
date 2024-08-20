7eaccc784aab937b972fb457f7200c38c6f0735a
kos5667 <kos5667@gmail.com>
2024-08-21 08:53:08 +0900

    Q10809.java - 완료

--- algorithm/BAEKJOON/A_bronze/Q10809.java
+++ algorithm/BAEKJOON/A_bronze/Q10809.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 /**
  * 1152 | 단어의 개수
- *  * 시간 제한 2초 | 메모리 제한 128 MB | 브론즈2
+ * 시간 제한 2초 | 메모리 제한 128 MB | 브론즈2
  */
 public class Q10809 {
     public static void main(String[] args) throws IOException {
