09f75eff1e0d17e9300dd3b3f09210db56964926
kos5667 <kos5667@gmail.com>
2024-08-21 23:22:32 +0900

    Q11660.java - 진행중

--- algorithm/BAEKJOON/B_silver/Q11660.java
+++ algorithm/BAEKJOON/B_silver/Q11660.java
@@ -21,13 +21,26 @@ public class Q11660 {
         int n = Integer.parseInt(tokenizer.nextToken());
         int m = Integer.parseInt(tokenizer.nextToken());
 
+        /**
+         * [1,1] [1,2] [1,3] [1,4]  설명)
+         * [2,1] [2,2] [2,3] [2,4]  1. i == 0 && j == 0 1행 구하고 j > 0 이후로 1행 합 배열 구하기
+         * [3,1] [3,2] [3,3] [3,4]  2-1. i > 0 && j == 0 1열 구하기
+         * [4,1] [4,2] [4,3] [4,4]  2-2. ex) [3,2] == [3,1] + [2,2] + num - [2,1]
+         */
         int[][] s = new int[n][n];
         for (int i=0; i<n; i++) {
             tokenizer = new StringTokenizer(br.readLine());
             for (int j=0; j<n; j++) {
-                s[i][j] = Integer.parseInt(tokenizer.nextToken());
+                int num = Integer.parseInt(tokenizer.nextToken());
+                if (i == 0) {
+                    s[i][j] = (j == 0) ? num : s[i][j-1] + num;
+                } else {
+                    s[i][j] = (j == 0) ? s[i-1][j] + num : s[i][j-1] + s[i-1][j] + num - s[i-1][j-1];
+                }
             }
         }
+
+
 
 
 
