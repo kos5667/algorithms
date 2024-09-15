f34eedae40f49c61b3411b98fe91a038c7807744
kos5667 <kos5667@gmail.com>
2024-09-15 12:32:12 +0900

    READMETEST.md 임시 테스트파일 생성. ReadmeBulider.java - sub package 추출

--- ReadmeBulider.java
+++ ReadmeBulider.java
@@ -1,23 +1,43 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class ReadmeBulider {
 
     static final String GOLD = "\uD83E\uDD47";
-    public static void main(String[] args) {
-        String filePath = "./README.md";
-        String questionPath = "./algorithm/BAEKJOON/Done/*";
 
-        String test = "dddd";
+    static final String basePackage = "./algorithm/BAEKJOON/Done";
+    public static void main(String[] args) throws IOException {
+        String filePath = "./READMETEST.md";
 
-        System.out.println(GOLD);
 
+        System.out.println(getSubPackages());
 //        try {
 //            Files.write(Paths.get(filePath), test.getBytes(), StandardOpenOption.APPEND);
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+    }
+
+    public static List<String> getSubPackages() {
+        List<String> subPackages = new ArrayList<>();
+        File baseDir = new File(basePackage);
+
+        if (baseDir.exists() && baseDir.isDirectory()) {
+            File[] files = baseDir.listFiles(File::isDirectory);
+            if (files != null) {
+                for (File file : files) {
+                    String subPackage = basePackage + '.' + file.getName();
+                    subPackages.add(subPackage);
+                }
+            }
+        }
+        return subPackages;
     }
 }
