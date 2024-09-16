02790c1ba8ee4bed9a9ce78ebd202499f3c0a468
kos5667 <kos5667@gmail.com>
2024-09-16 15:00:11 +0900

    완전 기초 내용 정리

--- algorithm/BAEKJOON/Done/Bronze/Main.java
+++ algorithm/BAEKJOON/Done/Bronze/Main.java
@@ -1,16 +1,36 @@
 package Done.Bronze;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        double a = Integer.parseInt(st.nextToken());
-        double b = Integer.parseInt(st.nextToken());
+        main.main();
+        Q1000.main();
+    }
 
-        System.out.println("Hello World!");
+    public static class main {
+        public static void main () throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            double a = Integer.parseInt(st.nextToken());
+            double b = Integer.parseInt(st.nextToken());
+
+            System.out.println("Hello World!");
+        }
+    }
+
+    public static class Q1000 {
+        public static void main() {
+            try (Scanner scan = new Scanner(System.in)) {
+                int a = scan.nextInt();
+                int b = scan.nextInt();
+
+                System.out.println(a + b);
+            }
+        }
     }
 }
