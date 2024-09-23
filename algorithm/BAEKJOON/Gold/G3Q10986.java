68df1c84b08bc8f5cae9e54e21bcf8ce029c5f51
kos5667 <kos5667@gmail.com>
2024-09-23 22:46:17 +0900

    ReadmeBulider.java

--- algorithm/BAEKJOON/Gold/G3Q10986.java
+++ algorithm/BAEKJOON/Gold/G3Q10986.java
@@ -1,9 +1,7 @@
 package Gold;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 /**
  * @questionNo 10986
@@ -14,19 +12,64 @@ import java.util.StringTokenizer;
  */
 public class G3Q10986 {
 
-    /**
+    /** 1 5 3 2 5
      * 직접 구현
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-    }
-
-    public static void print (long[][] s) {
-        for (long[] i : s) {
-            for (long j : i) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
+        long[] s = new long[n];
+        for (int i=0; i<n; i++) {
+            s[i] = i == 0 ? sc.nextInt() : s[i-1] + sc.nextInt();
         }
+
+        long[] c = new long[m];
+        for (long i : s) {
+            c[(int) (i % m)]++;
+        }
+
+        long answer = c[0];
+        for (int i=0; i<m; i++) {
+            answer += c[i] * (c[i] - 1) / 2;
+        }
+        System.out.println(answer);
     }
+
+    /**
+     * 교제
+     */
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//
+//        long[] s = new long[n];
+//        long[] c = new long[m];
+//        long answer = 0;
+//        s[0] = sc.nextInt();
+//        for (int i=1; i<n; i++) {
+//            s[i] = s[i-1] + sc.nextInt();
+//        }
+//
+//        for (int i=0; i<n; i++) {
+//            int remainder = (int) (s[i] % m);
+//            // 0 ~ i 까지의 구간 합 자체가 0일 때 정답에 더하기
+//            if (remainder == 0) answer++;
+//            // 나머지가 같은 인덱스의 개수 카운팅
+//            c[remainder]++;
+//        }
+//
+//        for (long i : c) {
+//            System.out.print(i + " ");
+//        }
+//
+//        for (int i=0; i<m; i++) {
+//            if (c[i] > 1) {
+//                answer = answer + (c[i] * (c[i] -1) / 2);
+//            }
+//        }
+//        System.out.println(answer);
+//    }
 }
