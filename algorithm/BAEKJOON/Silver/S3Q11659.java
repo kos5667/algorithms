a1636112838c5b2287f4fcbe8093970c990dd838
kos5667 <kos5667@gmail.com>
2025-01-27 16:37:32 +0900

    S5Q2018.java - 복습

--- algorithm/BAEKJOON/Silver/S3Q11659.java
+++ algorithm/BAEKJOON/Silver/S3Q11659.java
@@ -11,37 +11,72 @@ import java.util.StringTokenizer;
  * @questionLevel 3
  * @isComplete true
  * @since 2024-08-19
+ * @desription
+ * 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+ *
+ * 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다.
+ * 수는 1,000보다 작거나 같은 자연수이다. 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+ *
  */
 public class S3Q11659 {
 
-    /**
-     *
-     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
         st = new StringTokenizer(br.readLine());
+
         long[] arr = new long[n+1];
         for (int i=0; i<n; i++) {
             arr[i+1] = i == 0 ? Integer.parseInt(st.nextToken()) : arr[i] + Integer.parseInt(st.nextToken());
         }
 
-        long[] sum = new long[m];
         for (int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sum[i] = arr[b] - arr[a-1];
+            System.out.println(arr[b] - arr[a-1]);
         }
 
-        for (int i=0; i<sum.length; i++) {
-            System.out.println(sum[i]);
-        }
+        // 5 4 3 2 1
+        // 5 9 12 14 15
+        // 1~3 12
+        // 2~4 9
+        // 5~5 1
     }
+
+
+
+    /**
+     *
+     */
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int n = Integer.parseInt(st.nextToken());
+//        int m = Integer.parseInt(st.nextToken());
+//
+//        st = new StringTokenizer(br.readLine());
+//        long[] arr = new long[n+1];
+//        for (int i=0; i<n; i++) {
+//            arr[i+1] = i == 0 ? Integer.parseInt(st.nextToken()) : arr[i] + Integer.parseInt(st.nextToken());
+//        }
+//
+//        long[] sum = new long[m];
+//        for (int i=0; i<m; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            sum[i] = arr[b] - arr[a-1];
+//        }
+//
+//        for (int i=0; i<sum.length; i++) {
+//            System.out.println(sum[i]);
+//        }
+//    }
 
     /**
      * 복습 1
