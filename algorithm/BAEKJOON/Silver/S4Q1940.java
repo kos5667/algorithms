8ce0264c09ea64b365e95979826d5319a9ccf7ba
kos5667 <kos5667@gmail.com>
2025-09-04 22:25:09 +0900

    S4Q1940.java

---
+++ algorithm/BAEKJOON/Silver/S4Q1940.java
@@ -0,0 +1,49 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @questionNo 1940
+ * @questionTitle 주몽의 명령
+ * @questionLevel 4
+ * @isComplete true
+ * @since 2025-09-04
+ * @desription
+ * 주몽은 철기군을 양성하기 위한 프로젝트에 나섰다. 그래서 야철대장을 통해 철기군이 입을 갑옷을 만들게 하였다. 야철대장은 주몽의 명에 따르기 위하여 연구에 착수하던 중 아래와 같은 사실을 발견하게 되었다.
+ *
+ * 갑옷을 만드는 재료들은 각각 고유한 번호를 가지고 있다. 갑옷은 두 개의 재료로 만드는데 두 재료의 고유한 번호를 합쳐서 M(1 ≤ M ≤ 10,000,000)이 되면 갑옷이 만들어 지게 된다.
+ * 야철대장은 자신이 만들고 있는 재료를 가지고 갑옷을 몇 개나 만들 수 있는지 궁금해졌다. 이러한 궁금증을 풀어 주기 위하여 N(1 ≤ N ≤ 15,000) 개의 재료와 M이 주어졌을 때 몇 개의 갑옷을 만들 수 있는지를 구하는 프로그램을 작성하시오.
+ */
+public class S4Q1940 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int cnt = 0;
+        int i = 0;
+        int j = N-1;
+        while (i<j) {
+            if (arr[i] + arr[j] < M) {
+                i++;
+            } else if (arr[i] + arr[j] > M) {
+                j--;
+            } else {
+                cnt++;
+                i++; j--;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
