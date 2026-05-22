4b7d6c6544165fd797fa92adafeb711c8aa51080
kos5667 <kos5667@gmail.com>
2026-05-23 00:36:30 +0900

    TwoSum - 성공
    https://leetcode.com/problems/two-sum/description/

---
+++ algorithm/leetcode/TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new TwoSum().twoSum(new int[]{11, 7, 2, 15}, 9);
+    }
+}
