4b78ca605ea4df8fdac8663e8afd8caa9a9bc715
kos5667 <kos5667@gmail.com>
2026-05-22 16:58:05 +0900

    Q147354.java
    https://school.programmers.co.kr/learn/courses/30/lessons/147354

---
+++ algorithm/programmers/level2/Q147354.java
@@ -0,0 +1,29 @@
+package level2;
+
+import java.util.Arrays;
+
+public class Q147354 {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        Arrays.sort(data, (a, b) -> {
+            if (a[col-1] != b[col-1]) {
+                return a[col - 1] - b[col - 1];
+            }
+            return b[0] - a[0];
+        });
+
+        int answer = 0;
+        for (;row_begin <= row_end; row_begin++) {
+            int finalRow_begin = row_begin;
+            answer ^= Arrays.stream(data[row_begin - 1])
+                    .map(i -> i % finalRow_begin)
+                    .sum();
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] data = {{2,2,6},{1,5,10},{4,2,9},{3,8,3}};
+        new Q147354().solution(data, 2, 2 ,3);
+    }
+}
