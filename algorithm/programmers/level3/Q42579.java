7c8813cafdfad282f9921c6fbe1abff9d30dd411
kos5667 <kos5667@gmail.com>
2026-05-23 00:07:16 +0900

    Q42579.java - 실패
    https://school.programmers.co.kr/learn/courses/30/lessons/42579

--- algorithm/programmers/level3/Q42579.java
+++ algorithm/programmers/level3/Q42579.java
@@ -4,37 +4,36 @@ import java.util.*;
 
 public class Q42579 {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> genreSort = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        int[][] playsSorted = new int[plays.length][2];
+        for (int i = 0; i < genres.length; i++) {
+            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
 
-        int[][] sort = new int[genres.length][2];
-        for (int i = 0; i < plays.length; i++) {
-            sort[i][0] = i;
-            sort[i][1] = plays[i];
-
-            genreSort.put(genres[i], genreSort.getOrDefault(genres[i], 0) + plays[i]);
+            playsSorted[i][0] = i;
+            playsSorted[i][1] = plays[i];
         }
 
-        String[] sortedG = genreSort.entrySet().stream()
-                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+        String[] sorted = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue((o1, o2) -> o2 - o1))
                 .map(Map.Entry::getKey)
                 .toArray(String[]::new);
 
-        Arrays.sort(sort, ((o1, o2) -> o2[1] - o1[1]));
+        Arrays.sort(playsSorted, ((o1, o2) -> o2[1] - o1[1]));
 
-        int idx = 0;
-        int[] answer = new int[sortedG.length * 2];
+        int[] answer = new int[sorted.length * 2];
+        int a = 0;
+        for (int s=0; s < sorted.length; s++) {
+            int idx = 0;
+            for (int i=0; i < plays.length; i++) {
+                if (idx == 2) break;
 
-        for (String genre : sortedG) {
-            for (int[] ints : sort) {
-                if (idx == sortedG.length * 2)
-                    break;
-
-                if (genre.equals(genres[ints[0]])) {
-                    answer[idx] = ints[0];
+                if (sorted[s].equals(genres[playsSorted[i][0]])) {
+                    answer[a++] = playsSorted[i][0];
                     idx++;
                 }
             }
         }
+
         return answer;
     }
 
