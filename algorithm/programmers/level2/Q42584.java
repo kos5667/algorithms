870b23e330a03e580ee6a7a1bf2da5844a2a2441
kos5667 <kos5667@gmail.com>
2026-05-24 21:21:04 +0900

    Q42584 - O(n^2) -> O(n) 수정 recovery: 0 time: 0h 30m memo: O(n) 수정 추가. 복기 필요.

--- algorithm/programmers/level2/Q42584.java
+++ algorithm/programmers/level2/Q42584.java
@@ -3,6 +3,7 @@ package level2;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @title 주식가격
@@ -27,32 +28,99 @@ import java.util.Queue;
  * 5초 시점의 ₩3은 0초간 가격이 떨어지지 않았습니다.
  */
 public class Q42584 {
+    /**
+     * 정확성  테스트
+     * 테스트 1 〉	통과 (0.03ms, 59.4MB)
+     * 테스트 2 〉	통과 (0.25ms, 63.6MB)
+     * 테스트 3 〉	통과 (0.95ms, 62.1MB)
+     * 테스트 4 〉	통과 (0.95ms, 60.6MB)
+     * 테스트 5 〉	통과 (0.98ms, 62.5MB)
+     * 테스트 6 〉	통과 (0.11ms, 62.1MB)
+     * 테스트 7 〉	통과 (0.85ms, 62.9MB)
+     * 테스트 8 〉	통과 (1.24ms, 64.5MB)
+     * 테스트 9 〉	통과 (0.14ms, 62.3MB)
+     * 테스트 10 〉	통과 (1.18ms, 62.3MB)
+     * 효율성  테스트
+     * 테스트 1 〉	통과 (22.03ms, 76.7MB)
+     * 테스트 2 〉	통과 (20.42ms, 72.2MB)
+     * 테스트 3 〉	통과 (23.58ms, 77MB)
+     * 테스트 4 〉	통과 (24.37ms, 79.3MB)
+     * 테스트 5 〉	통과 (20.38ms, 72MB)
+     */
+//    public int[] solution(int[] prices) {
+//        Queue<int[]> queue = new ArrayDeque<>();
+//        for (int i = 0; i < prices.length; i++) {
+//            queue.add(new int[] {i, prices[i], 0});
+//        }
+//
+//        int[] answer = new int[prices.length];
+//        while (!queue.isEmpty()) {
+//            int[] price = queue.poll();
+//
+//            for (int[] p : queue) {
+////                if (price[1] <= p[1]) price[2]++;
+//
+//                price[2]++;
+//                if (price[1] > p[1]) {
+//                    break;
+//                }
+//            }
+//            answer[price[0]] = price[2];
+//        }
+//        return answer;
+//    }
+
+    /**
+     * GPT
+     *
+     * 정확성  테스트
+     * 테스트 1 〉	통과 (0.10ms, 61.3MB)
+     * 테스트 2 〉	통과 (0.62ms, 59.7MB)
+     * 테스트 3 〉	통과 (0.83ms, 61.7MB)
+     * 테스트 4 〉	통과 (0.89ms, 61.1MB)
+     * 테스트 5 〉	통과 (0.87ms, 61.1MB)
+     * 테스트 6 〉	통과 (0.16ms, 65.8MB)
+     * 테스트 7 〉	통과 (0.81ms, 61.2MB)
+     * 테스트 8 〉	통과 (0.85ms, 60.4MB)
+     * 테스트 9 〉	통과 (0.15ms, 63.1MB)
+     * 테스트 10 〉	통과 (0.97ms, 61.7MB)
+     * 효율성  테스트
+     * 테스트 1 〉	통과 (14.50ms, 76.6MB)
+     * 테스트 2 〉	통과 (12.47ms, 74.8MB)
+     * 테스트 3 〉	통과 (31.60ms, 81.7MB)
+     * 테스트 4 〉	통과 (11.91ms, 73MB)
+     * 테스트 5 〉	통과 (8.71ms, 66.7MB)
+     */
     public int[] solution(int[] prices) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        for (int i = 0; i < prices.length; i++) {
-            queue.add(new int[] {i, prices[i], 0});
-        }
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
 
-        int[] answer = new int[prices.length];
-        while (!queue.isEmpty()) {
-            int[] price = queue.poll();
-
-            for (int[] p : queue) {
-//                if (price[1] <= p[1]) price[2]++;
-
-                price[2]++;
-                if (price[1] > p[1]) {
-                    break;
-                }
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
             }
-            answer[price[0]] = price[2];
+
+            stack.push(i);
+//            System.out.println(Arrays.toString(answer) + " " + stack);
+//            System.out.println(Arrays.toString(answer));
+//            System.out.println(stack);
         }
+
+        System.out.println(Arrays.toString(answer) + " " + stack);
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = n - 1 - index;
+        }
+        System.out.println(Arrays.toString(answer));
         return answer;
     }
 
     public static void main(String[] args) {
-        new Q42584().solution(new int[] {1, 2, 3, 2, 3});
-        new Q42584().solution(new int[] {1, 1});
+//        new Q42584().solution(new int[] {1, 2, 3, 2, 3});
+//        new Q42584().solution(new int[] {1, 1});
         new Q42584().solution(new int[] {5, 4, 3, 2, 1});
     }
 }
