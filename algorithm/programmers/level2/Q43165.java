8dbe554ae985cb172bdf7eb0a6eb54ffeb7946e0
kos5667 <kos5667@gmail.com>
2026-05-27 21:33:53 +0900

    Q43165 - 복기 title: 타겟 넘버 recovery: 2 time: 0h 30m memo: dfs 복기

--- algorithm/programmers/level2/Q43165.java
+++ algorithm/programmers/level2/Q43165.java
@@ -38,20 +38,22 @@ numbers	target	return
 public class Q43165 {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        return answer;
+        dfs(numbers, target, 1, +numbers[0]);
+        dfs(numbers, target, 1, -numbers[0]);
+        System.out.println(answer);
+        return 0;
     }
 
-    public void dfs(int [] numbers, int target, int index, int sum) {
-        if (index == numbers.length) {
+    private void dfs(int[] numbers, int target, int idx, int sum) {
+        if ((idx) == numbers.length) {
             if (sum == target) {
                 answer++;
             }
             return;
         }
 
-        dfs(numbers, target, index + 1, sum + numbers[index]);
-        dfs(numbers, target, index + 1, sum - numbers[index]);
+        dfs(numbers, target, idx+1, sum + numbers[idx]);
+        dfs(numbers, target, idx+1, sum - numbers[idx]);
     }
 
     public static void main(String[] args) {
