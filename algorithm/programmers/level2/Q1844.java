6016544626f9214795781b3f52f9c4cdaa0b4fa6
kos5667 <kos5667@gmail.com>
2026-05-27 13:56:01 +0900

    Q1844 - 복기 title: 게임 맵 최단거리 recovery: 1 time: 0h 30m memo: 시간초과 이슈 해결.

--- algorithm/programmers/level2/Q1844.java
+++ algorithm/programmers/level2/Q1844.java
@@ -1,8 +1,6 @@
 package level2;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java">...</a>
@@ -18,55 +16,46 @@ public class Q1844 {
         int[] dy = {0, 0, -1, 1};
 
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0, 0}); // 시작은 (0, 0);
+        queue.offer(new int[]{0, 0});
 
         boolean[][] visited = new boolean[n][m];
 
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+            int[] curr = queue.poll();
 
-            int x = current[0];
-            int y = current[1];
+            int x = curr[0];
+            int y = curr[1];
 
-            // action은 4방향 위 아래 왼쪽 오른쪽
-            for (int i=0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nextX = dx[i] + x;
+                int nextY = dy[i] + y;
 
-                /**
-                 * nx는 0보다 작을 수 없다.
-                 * ny는 0보다 작을 수 없다.
-                 * n, y 또한 현재보다 커야 함.
-                 */
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                // 벽 외로 이동 금지.
+                if (nextY < 0 || nextX < 0 || nextX >= n || nextY >= m) {
                     continue;
                 }
 
-                // 방문했으면 pass
-                if (visited[nx][ny]) {
+                if (visited[nextX][nextY]) {
                     continue;
                 }
 
-                // 막힌 길 pass
-                if (maps[nx][ny] == 0) {
+                // 막힌 벽 금지.
+                if (maps[nextX][nextY] == 0) {
                     continue;
                 }
 
-                visited[nx][ny] = true;
-                maps[nx][ny] = maps[x][y] + 1;
+                visited[nextX][nextY] = true;
+                maps[nextX][nextY] = maps[x][y] + 1;
 
-                queue.offer(new int[]{nx, ny});
+                queue.offer(new int[]{nextX, nextY});
             }
         }
 
-        for (int[] i : maps) {
-            System.out.println(Arrays.toString(i));
-        }
         return maps[n - 1][m - 1] == 1 ? -1 : maps[n - 1][m - 1];
     }
 
     public static void main(String[] args) {
         new Q1844().solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}});
-//        new Q1844().solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}});
+        new Q1844().solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}});
     }
 }
